893d33fa1dd49ebc2ad96b56ed1e4a5654965ed8
Анатолий Шипилов <asmatol@yandex.ru>
2025-06-10 10:19:28 +0300

    added sidebar and textArea and custom component Icon

---
+++ src/shared/ui/icon/Icon.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import sprite from "./sprite.svg";
+
+type IconPropsType = {
+  iconId: string
+  width?: string
+  height?: string
+  viewBox?: string
+  fill?: string
+  stroke?: string
+  strokeWidth?: string
+
+}
+
+export const Icon = (props:IconPropsType) => {
+  const {width, height, viewBox, fill, stroke, iconId, strokeWidth} = props
+
+  return (
+    <svg
+      strokeWidth={strokeWidth || 'inherit'}
+      width={width || '24'}
+      height={height || '24'}
+      viewBox={viewBox || '0 0 24 24'}
+      fill={fill || 'none'}
+      stroke={stroke || 'none'}
+      xmlns="http://www.w3.org/2000/svg">
+      <use xlinkHref={`${sprite.src}#${iconId}`} />
+    </svg>
+  );
+};
---
+++ src/shared/ui/sideBar/sideBarItem/SideBarItem.tsx
@@ -0,0 +1,21 @@
+import s from './SideBarItem.module.scss'
+import { Icon } from '@/shared/ui/icon/Icon'
+
+
+type SideBarItem = {
+  iconId: string
+  name: string
+  href: string
+}
+
+
+
+export const SideBarItem = ({iconId, name, href} : SideBarItem) => {
+
+  return (
+    <a href={href} className={s.flexWrapper}>
+      <Icon iconId={iconId} />
+      <span>{name}</span>
+    </a>
+  )
+}
