915c75e9f2de5fe168552a023fe144884c4a1633
Анатолий Шипилов <asmatol@yandex.ru>
2025-06-20 02:03:32 +0300

    added date-picker only check one date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 import styles from './page.module.css'
+import { Picker } from '@/shared/ui/datePicker/Picker'
 
 export default function Home() {
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Главная страница</h1>
       </main>
+      <Picker />
     </div>
   )
 }
