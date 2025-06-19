65658f8531a4e1ca4245f6616978d73b94162f64
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-06-19 20:12:35 +0200

    feat: add icons, add common auth schemas

---
+++ src/features/auth/lib/schemas/CommonAuthSchemas.ts
@@ -0,0 +1,16 @@
+import z from 'zod'
+
+export const emailSchema =  z.string().email({
+  message: 'The email must match the format example@example.com',
+})
+
+export const passwordSchema =  z
+  .string()
+  .min(6, { message: 'Minimum number of characters 6' })
+  .max(30, { message: 'Maximum number of characters 20' })
+  .refine((val) => /[0-9]/.test(val) && /[a-z]/.test(val) && /[A-Z]/.test(val) && /[!"#$%&'()*+-./:;<=>?@[\]^_`{|}~]/.test(val),{
+    message:
+      'Password must contain 0-9, a-z, A-Z, ! " # $ % & \' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _` { | } ~}',
+  })
+
+
