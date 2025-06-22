05164197226962e75c2d3b7b78cf3fbdf1e602bb
lopatop <pavellopato327@gmail.com>
2025-06-22 21:48:23 +0200

    IN-74-OAuth feat: GitHubLoginButton

---
+++ src/shared/ui/OAuth/GitHubLoginButton/GitHubLoginButton.tsx
@@ -0,0 +1,21 @@
+import GithubSvgrepoCom31 from '@/assets/icons/components/GithubSvgrepoCom31'
+
+export const GitHubLoginButton = () => {
+  const CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID
+  //const REDIRECT_URL = process.env.NEXT_PUBLIC_GITHUB_REDIRECT_URL
+  const REDIRECT_URL_DEV = process.env.NEXT_PUBLIC_GITHUB_REDIRECT_URL_DEV
+
+  const AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_DEV}&scope=user:email`
+
+  const login = () => {
+    window.location.assign(AUTH_URL)
+  }
+
+  return (
+    <div>
+      <button onClick={login}>
+        <GithubSvgrepoCom31 />
+      </button>
+    </div>
+  )
+}
