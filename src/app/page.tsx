bdc04ec70594244ba41a85cc6cfcd51c26fec17c
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-06-22 22:01:18 +0200

    feat: add store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
 'use client'
 import styles from './page.module.css'
+import { useRegistrationConfirmationMutation } from '@/features/auth/api/registrationApi'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function Home() {
+
+   const router = useRouter()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    const code = searchParams?.get('code')
+    const email = searchParams?.get('email')
+
+    if (code && email) {
+      router.replace(`/auth/signup?code=${code}&email=${email}`)
+    }
+  }, [searchParams, router])
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
