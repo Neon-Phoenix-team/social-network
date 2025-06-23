91bdf12a533f1771063bd80e318c26f611209e65
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-06-23 19:38:50 +0200

    feat: add email verification process

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,35 +1,9 @@
 'use client'
 
 import { RegisterForm } from '@/features/auth/ui/RegisterForm/RegisterForm'
-import { useEffect, useState } from 'react'
-import { useSearchParams } from 'next/navigation'
-import { useRegistrationConfirmationMutation } from '@/features/auth/api/registrationApi'
 
 const SignupPage = () => {
-  const searchParams = useSearchParams()
-  const code = searchParams?.get('code')
-  const email = searchParams?.get('email')
-
-  const [mode, setMode] = useState<'confirm' | 'register'>('register')
-
-  const [confirm,{isLoading}] = useRegistrationConfirmationMutation()
-
-  useEffect(() => {
-    if (code && email) {
-      confirm(code).then(()=> {
-        setMode('confirm')
-      }).catch((err) =>{
-        console.log(err)
-      })
-
-    }
-  }, [confirm,code, email])
-  
-  if (isLoading) {
-    return <p>загрузка</p>
-  }
-
-  return mode === 'confirm' ? <p>confirm</p> : <RegisterForm />
-
+  return  <RegisterForm />
 }
+
 export default SignupPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
 'use client'
 import styles from './page.module.css'
-import { useRegistrationConfirmationMutation } from '@/features/auth/api/registrationApi'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
+import { EmailVerification } from '@/shared/ui/EmailVerification/EmailVerification'
+import { useRegistrationConfirmationMutation } from '@/features/auth/api/registrationApi'
 
 export default function Home() {
-
-   const router = useRouter()
   const searchParams = useSearchParams()
-
+  const code = searchParams?.get('code')
+  const [confirm,{isSuccess}] = useRegistrationConfirmationMutation()
   useEffect(() => {
-    const code = searchParams?.get('code')
-    const email = searchParams?.get('email')
-
-    if (code && email) {
-      router.replace(`/auth/signup?code=${code}&email=${email}`)
+    if (code) {
+      confirm(code)
+        .unwrap()
+        .catch(err => {
+          console.log(err)
+        })
     }
-  }, [searchParams, router])
+  }, [confirm, code])
+
+  if (code) {
+    return <EmailVerification showForm isSuccess={isSuccess} />
+  }
 
   return (
     <div className={styles.page}>
