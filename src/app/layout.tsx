b376a2e608581f083a008f28604ef5f645aa442e
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-06-23 19:36:36 +0200

    refactor: delete unused fonts fix: add storeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import '@/shared/styles/global/globals.css'
 import { Header } from '@/shared/ui/header/Header'
 import Menu from '@/shared/ui/Menu/Menu'
+import { StoreProvider } from '@/shared/ui/StoreProvider/StoreProvider'
+import { ReactNode } from 'react'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -22,16 +16,18 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body >
+      <StoreProvider >
         <Header />
         <div className="scroll-container">
           <Menu />
           <main className="content">{children}</main>
         </div>
+      </StoreProvider>
       </body>
     </html>
   )
