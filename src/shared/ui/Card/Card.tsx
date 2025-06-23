eda8970210675585fcde3667f02c461cd470412a
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-06-23 17:41:56 +0200

    fix: change Card props

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -3,41 +3,36 @@
 import { Button } from '@/shared/ui/Button/Button'
 import s from './Card.module.scss'
 import { CloseIcon } from '@/shared/assets/icons/common/Close'
-
-
+import { ReactNode } from 'react'
 
 export type CardTextType = {
+  open: boolean
   title: string
-  text: string
   action: () => void
+  children: ReactNode
 }
-export const Card = ({ title,text,action }: CardTextType) => {
-
-
-  const onClickHandler = () => {
-    action()
-  }
-
+export const Card = ({ title,action, open,children }: CardTextType) => {
   return (
     <>
-      <div className={s.overlay} />
-
-      <div className={s.wrapper}>
-        <div className={s.title}>
-          <span>{title}</span>
-          <Button
-            onClick={onClickHandler}
-            variant={'text'}
-            style={{ color: '#fff' }}
-          >
-            <CloseIcon />
-          </Button>
+      {open && (
+        <div className={s.overlay}>
+          <div className={s.wrapper}>
+            <div className={s.title}>
+              <span>{title}</span>
+              <Button
+                onClick={action}
+                variant={'text'}
+                style={{ color: '#fff' }}
+              >
+                <CloseIcon />
+              </Button>
+            </div>
+            <div className={s.textWrapper}>
+              {children}
+            </div>
+          </div>
         </div>
-        <div className={s.textWrapper}>
-          <span className={s.text}>{text}</span>
-          <Button onClick={onClickHandler}>OK</Button>
-        </div>
-      </div>
+      )}
     </>
   )
 }
