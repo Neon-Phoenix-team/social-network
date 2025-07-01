635d741448136c4a17470c9b7fe0ee5437efa823
Michael Dudzik <mkdudik15@gmail.com>
2025-07-01 16:50:56 +0300

    add conditional rendering of root page for logged in user or not

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 import { EmailVerification } from '@/shared/ui/EmailVerification/EmailVerification'
 import { useRegistrationConfirmationMutation } from '@/features/auth/api/registrationApi'
+import { useGetMeQuery } from '@/features/auth/signin/model/signInApi'
+import Menu from '@/shared/ui/Menu/Menu'
 
 export default function Home() {
   const router = useRouter()
   const searchParams = useSearchParams()
   const code = searchParams?.get('code')
-
+  const { data: user, isLoading, isError: isAuthError } = useGetMeQuery()
+  const isLoggedIn = !!user
   const email = searchParams?.get('email')
 
   if (email && code) {
@@ -33,11 +36,29 @@ export default function Home() {
   if (code && (isSuccess || isError)) {
     return <EmailVerification showForm isSuccess={isSuccess} />
   }
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div className={styles.page}>
+      {isLoggedIn && (
+        <div className={styles.menuWrapper}>
+          <Menu />
+        </div>
+      )}
       <main className={styles.main}>
-        <h1>Главная страница</h1>
+        {isLoggedIn ? (
+          <>
+            <h1>Добро пожаловать, {user.userName}!</h1>
+            <p>Ваш email: {user.email}</p>
+          </>
+        ) : (
+          <>
+            <h1>Главная страница</h1>
+            <p>Пожалуйста, войдите или зарегистрируйтесь</p>
+          </>
+        )}
       </main>
     </div>
   )
