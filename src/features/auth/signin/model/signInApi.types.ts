99a398ee1b5351cdbd6bcd5b2746c21a945dc126
Michael Dudzik <mkdudik15@gmail.com>
2025-07-01 16:51:43 +0300

    add me response type

--- src/features/auth/signin/model/signInApi.types.ts
+++ src/features/auth/signin/model/signInApi.types.ts
@@ -2,6 +2,14 @@ export interface SignInArgs {
   email: string
   password: string
 }
+
 export interface SignInResponse {
   accessToken: 'string'
+}
+
+export interface MeResponse {
+  userId: number
+  userName: string
+  email: string
+  isBlocked: boolean
 }
