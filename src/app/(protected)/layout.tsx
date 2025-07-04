281eb78a2e77d5ab72b79437c1572298d28d7bbf
Michael Dudzik <mkdudik15@gmail.com>
2025-07-04 16:17:48 +0300

    relocate layout.tsx

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -11,7 +11,7 @@ export default function ProtectedLayout({ children }: { children: ReactNode }) {
 
   return (
     <>
-      <StoreProvider>{children}</StoreProvider>
+      <StoreProvider> {children}</StoreProvider>
     </>
   )
 }
